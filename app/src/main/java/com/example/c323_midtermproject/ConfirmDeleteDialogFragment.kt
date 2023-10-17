de4424de9096c14ed8443e2c0c63a770578d994b
egoel5 <egoel13@gmail.com>
2023-10-17 03:22:48 -0400

    Update Midterm Project

---
+++ app/src/main/java/com/example/c323_midtermproject/ConfirmDeleteDialogFragment.kt
@@ -0,0 +1,41 @@
+package com.example.c323_midtermproject
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.DialogFragment
+
+class ConfirmDeleteDialogFragment(val scoreId : Long,val clickListener: (taskId: Long) -> Unit) : DialogFragment() {
+    val TAG = "ConfirmDeleteDialogFragment"
+    interface myClickListener {
+        fun yesPressed()
+    }
+
+    var listener: myClickListener? = null
+
+    /**
+     * Initialize a Fragment that will act as a Delete Confirmation
+     * Set message, positiveButton's text, and negative button's text
+     */
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setMessage(getString(R.string.delete_confirmation))
+            .setPositiveButton("Yes") { _,_ -> clickListener(scoreId)}
+            .setNegativeButton("No") { _,_ -> }
+            .create()
+
+    companion object {
+        const val TAG = "ConfirmDeleteDialogFragment"
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            listener = context as myClickListener
+        } catch (e: Exception) {
+            Log.d(TAG, e.message.toString())
+        }
+    }
+}
